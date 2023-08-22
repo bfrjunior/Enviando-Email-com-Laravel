855c63ed659ed7aec453b127be26151115da3f09
Fernando Rocha <beneditofernandor@gmail.com>
2023-08-22 01:59:55 -0300

    personalizando Job

---
+++ app/Jobs/AuthMail.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Mail\RegisterEmail;
+use Illuminate\Support\Facades\Mail;
+
+class AuthMail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Mail::to('Kobsdev2019@gmail.com')->queue(new RegisterEmail(
+            data: ['nome' => 'Fernando', 'mensagem' => 'Tudo bem com você ?']
+        ));
+    }
+}
